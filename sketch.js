fcd871f8d6b3ee30e7ef74a82dacbf78734029ed
martinlehocky <75577971+martinlehocky@users.noreply.github.com>
2020-12-20 17:44:22 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,52 @@
+var resetbutton;
+var buttonimage;
+var counter = 0;
+var resetbuttonImage;
+var button;
+
+function preload(){
+  
+  buttonimage = loadImage("button.png");
+  resetbuttonImage = loadImage("reset.png");
+  
+}
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  
+  button = createSprite(windowWidth /2 ,windowHeight /2 + 100,10,10);
+  button.addImage(buttonimage);
+  button.scale = 0.4;
+   
+  
+  resetbutton = createSprite(windowWidth /2, button.y - 150, 10, 10);
+  resetbutton.addImage(resetbuttonImage);
+  resetbutton.scale = 0.1;
+}
+
+function draw() {
+  background(220);
+  
+  textSize(100);
+  text(counter, windowWidth /2 - 30 , resetbutton.y - 100);
+  
+  if(touches.length > 0 && touches.y > 360 && touches.y <  480 && touches.x > 280 && touches.x < 400){
+    
+    counter = counter + 1;
+    
+  }
+     
+  //if()){
+    
+    
+    
+ // }
+  
+     drawSprites();
+  
+}
+  
+  
+  
+  
